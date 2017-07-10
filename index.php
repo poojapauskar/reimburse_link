2631e51cd5c831d6282596d0c21ba84e6fb74cc7
poojapauskar <git.poojapauskar@gmail.com>
2017-07-10 14:26:18 +0530

    Index page added

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+<body>
+
+<h1>SmarTally</h1>
+
+<?php
+
+
+$url = 'https://reimburse.herokuapp.com/check_trip_status/';
+$options = array(
+  'http' => array(
+    'header'  => array(
+                  'TRIP-ID: '.$_GET['trip_id'],
+                ),
+    'method'  => 'GET',
+  ),
+);
+$context = stream_context_create($options);
+$output = file_get_contents($url, false,$context);
+/*echo $output;*/
+$arr = json_decode($output,true);
+/*echo $arr['details']['Trip Status']['status'];*/
+if($arr['details']['Trip Status']['status'] == "approved"){
+  echo "Link Expired";
+}else{
+      $url = 'https://reimburse.herokuapp.com/mark_trip_as_reimbursed/';
+      $options = array(
+        'http' => array(
+          'header'  => array(
+                        'TRIP-ID: '.$_GET['trip_id'],
+                        'STATUS: approved'
+                      ),
+          'method'  => 'GET',
+        ),
+      );
+      $context = stream_context_create($options);
+      $output = file_get_contents($url, false,$context);
+      /*echo $output;*/
+      $arr = json_decode($output,true);
+      if($arr['details']['status'] == 200){
+       echo "Trip Approved";
+      }
+      
+}
+?>
+
+</body>
+</html>
